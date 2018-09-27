c699accbc8e535af79811c76a9740593c7cc5495
ArturKudla <arturkapes@gmail.com>
2018-09-27 20:47:30 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var count = 0;
+  var p = 1;
+  while (Math.pow(5, p) < number) {  
+    count = parseInt(number / Math.pow(5, p)) + count;
+    p++;  
+  }
+  return count;
 }
